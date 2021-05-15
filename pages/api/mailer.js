c919ecdb41d350929237ebce18d5e08db1a58fd8
vmwhoami <vmwhoami@gmail.com>
2021-05-15 18:02:50 +0300

    Test things with api

--- pages/api/mailer.js
+++ pages/api/mailer.js
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
+  console.log(req.method);
   res.status(200).json({ name: 'John Doe' });
 };
