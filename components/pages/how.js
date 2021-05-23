cb0b2df4c458c6f01347be79e08713f42aa7187f
Vitalie Melnic <vmwhoami@gmail.com>
2021-05-23 19:46:09 +0300

    Add animation for the why section

--- components/pages/how.js
+++ components/pages/how.js
@@ -2,22 +2,34 @@ import { useInView } from 'react-hook-inview'
 import { motion } from "framer-motion"
 
 import Iframe from 'react-iframe';
+
+const visible = { x: 0, y: 0, opacity: 1 }
+const nonvisible = { x: 0, y: 100, opacity: 0 }
+const trans = {
+  delay: 1,
+  default: { duration: .2 },
+}
+
+const transForVideo = {
+  delay: 2,
+  default: { duration: .3 },
+}
 const How = () => {
   const [ref, inView] = useInView()
-  console.log(inView);
+
   return (
     <div className="how" >
       <motion.div
-        animate={inView ? { x: 0, y: 0, opacity: 1 } : { x: 0, y: 100, opacity: 0 }}
-        transition={{
-          delay: 1,
-          default: { duration: .2 },
-        }}
-
+        animate={inView ? visible : nonvisible}
+        transition={trans}
         className="how__h1">
         <h1 className="text-center">Как проходит тренировка?</h1>
       </motion.div>
-      <div className="how__description">
+      <motion.div
+
+        animate={inView ? visible : nonvisible}
+        transition={trans}
+        className="how__description">
         <p>
           Вы занимаетесь с персональным тренером по видеосвязи.
         {' '}
@@ -26,9 +38,11 @@ const How = () => {
         <br />
         и немного свободного пространства (2 м от камеры).
       </p>
-      </div>
-      <div className="how__vidcont" ref={ref}>
-
+      </motion.div>
+      <motion.div
+        animate={inView ? visible : nonvisible}
+        transition={transForVideo}
+        className="how__vidcont" ref={ref}>
         <Iframe
           className="how__vid"
           src="https://www.youtube.com/embed/esUFUlA__as"
@@ -38,8 +52,7 @@ const How = () => {
 
         />
 
-
-      </div>
+      </motion.div>
     </div>
   )
 };
--- components/pages/why.js
+++ components/pages/why.js
@@ -1,25 +1,56 @@
-const Why = () => (
-  <div className="why">
-    <h2 className="why__heading white-color">Почему это удобно?</h2>
-    <p className="why__p white-color">Тренируйтесь в комфортном ритме когда угодно и где угодно</p>
+import { useInView } from 'react-hook-inview'
+import { motion } from "framer-motion"
 
-    <div className="why__container">
-      <div className="why__section">
-        <img className="why__img" src="/images/plan.png" alt="plan" />
-        <p className="white-color">План занятий составляем индивидуально: он учитывает ваш образ жизни, цели и физические возможности</p>
-      </div>
 
-      <div className="why__section">
-        <img className="why__img" src="/images/coin.png" alt="coin" />
-        <p className="white-color">Регулярная работа с личным тренером в онлайн-режиме стоит в несколько раз дешевле занятий с тренером в зале</p>
-      </div>
+const Why = () => {
+  const [ref, inView] = useInView()
+  const visible = { x: 0, y: 0, opacity: 1 }
+  const nonvisible1 = { x: -200, y: 100, opacity: 0 }
+  const nonvisible2 = { x: 0, y: 200, opacity: 0 }
+  const nonvisible3 = { x: 200, opacity: 0 }
+  const nonvisibleh = { y: -100, opacity: 0 }
 
-      <div className="why__section">
-        <img className="why__img" src="/images/trainer.png" alt="plan" />
-        <p className="white-color">Регулярная работа с личным тренером в онлайн-режиме стоит в несколько раз дешевле занятий с тренером в зале</p>
-      </div>
+  const trans = {
+    delay: 2,
+    default: { duration: .5 },
+  }
+  return (
+    <div ref={ref} className="why">
+      <motion.h2
+        animate={inView ? visible : nonvisibleh}
+        transition={trans}
+
+        className="why__heading white-color">Почему это удобно?</motion.h2>
+      <p className="why__p white-color">Тренируйтесь в комфортном ритме когда угодно и где угодно</p>
+
+      <motion.div className="why__container">
+        <motion.div
+          animate={inView ? visible : nonvisible1}
+          transition={trans}
+
+          className="why__section">
+          <img className="why__img" src="/images/plan.png" alt="plan" />
+          <p className="white-color">План занятий составляем индивидуально: он учитывает ваш образ жизни, цели и физические возможности</p>
+        </motion.div>
+
+        <motion.div
+          animate={inView ? visible : nonvisible2}
+          transition={trans}
+          className="why__section">
+          <img className="why__img" src="/images/coin.png" alt="coin" />
+          <p className="white-color">Регулярная работа с личным тренером в онлайн-режиме стоит в несколько раз дешевле занятий с тренером в зале</p>
+        </motion.div>
+
+        <motion.div
+          animate={inView ? visible : nonvisible3}
+          transition={trans}
+          className="why__section">
+          <img className="why__img" src="/images/trainer.png" alt="plan" />
+          <p className="white-color">Регулярная работа с личным тренером в онлайн-режиме стоит в несколько раз дешевле занятий с тренером в зале</p>
+        </motion.div>
+      </motion.div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Why;
