085ae86414a355f2f4e53aa352df3f66d479f7dd
vmwhoami <vmwhoami@gmail.com>
2025-05-21 23:58:51 +0300

    make ina face swap

--- components/pages/how.js
+++ components/pages/how.js
@@ -21,15 +21,16 @@ const How = () => {
   const [ref, inView] = useInView();
 
   return (
-    <section className="px-6 py-20 lg:py-32 bg-gradient-to-b from-white to-gray-50">
-      <div className="max-w-5xl mx-auto text-center space-y-10">
-        <motion.h1
+    <figure className="px-6 py-20 my:40 lg:py-32 bg-gradient-to-b from-white to-gray-50">
+      <motion.h1
           animate={inView ? visible : hidden}
           transition={textTransition}
-          className="text-4xl md:text-5xl font-bold text-gray-900 leading-tight"
+          className="text-2xl md:text-5xl font-bold text-gray-900 leading-tight"
         >
           Пора прокачать тело и дух на свежем воздухе
         </motion.h1>
+      <div className="max-w-5xl mx-auto text-center space-y-10">
+        
 
         <motion.p
           animate={inView ? visible : hidden}
@@ -49,10 +50,11 @@ const How = () => {
           animate={inView ? visible : hidden}
           transition={videoTransition}
           ref={ref}
-          className="aspect-video max-w-4xl mx-auto shadow-2xl rounded-xl overflow-hidden"
+          className="w-full h-full max-w-4xl mx-auto shadow-2xl rounded-xl overflow-hidden"
         >
+          
           <Iframe
-            className="w-full h-full"
+            className="w-full h-full md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
             src="https://www.youtube.com/embed/esUFUlA__as"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -61,7 +63,7 @@ const How = () => {
           />
         </motion.div>
       </div>
-    </section>
+    </figure>
   );
 };
 
